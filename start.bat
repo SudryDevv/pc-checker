6b14e38dfd3077f487a4e439b9045cece6ad87bf
Sudry <108238011+SudryDevv@users.noreply.github.com>
2024-03-13 22:06:43 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,8 +4,8 @@ echo Vérification de Node.js...
 node -v >nul 2>&1
 if %errorlevel% neq 0 (
     echo Node.js n'est pas installé. Installation en cours...
-    curl -o node_installer.msi https://nodejs.org/dist/v20.11.1/node-v20.11.1-x64.msi
-    start /wait msiexec /i node_installer.msi /quiet
+    bitsadmin /transfer "Téléchargement_NodeJS" https://nodejs.org/dist/v20.11.1/node-v20.11.1-x64.msi "%CD%\node_installer.msi"
+    start /wait msiexec /i "%CD%\node_installer.msi" /quiet
     echo Installation de Node.js terminée.
 ) else (
     echo Node.js est déjà installé.
