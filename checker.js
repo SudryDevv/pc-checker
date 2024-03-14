321a6825e9518208bf045ddacbd6a6cbe6c8ad42
Sudry <108238011+SudryDevv@users.noreply.github.com>
2024-03-14 17:19:58 +0100

    Update checker.js

--- checker.js
+++ checker.js
@@ -84,14 +84,14 @@ function checkDirectory(parentDir, directory) {
             console.log(colors.green("[+] Téléchargement de Eulen trouvé dans :", colors.yellow(fullPath), "|", colors.red(file.name)));
 
           } else if (file.name.startsWith('TZ')) {
-            if(file.name.endsWith('.zip')) {
+            if (file.name.endsWith('.zip')) {
               cheatsFounds++;
               console.log(colors.green("[+] Téléchargement de TZ trouvé dans :", colors.yellow(fullPath), "|", colors.red(file.name)));
             }
 
           } else if (file.name.startsWith('TZX')) {
 
-            if(file.name.endsWith('.zip')) {
+            if (file.name.endsWith('.zip')) {
               cheatsFounds++;
               console.log(colors.green("[+] TZX trouvé dans :", colors.yellow(fullPath), "|", colors.red(file.name)));
             }
@@ -112,11 +112,11 @@ function checkDirectory(parentDir, directory) {
               const fileSizeInKilobytes = fileSizeInBytes / 1024;
               const fileSizeInMegabytes = fileSizeInKilobytes / 1024;
 
-              if(fileSizeInMegabytes.toFixed(2) < 10) {
+              if (fileSizeInMegabytes.toFixed(2) < 10) {
                 cheatsFounds++;
                 console.log(colors.green("[+] TZ / HX trouvé dans :", colors.yellow(fullPath), "|", colors.red(file.name)));
               }
-              })
+            })
           } else if (file.name.includes('visualstudio.exe') && file.name.includes('edge.exe') && file.name.includes('eclipse.exe') && file.name.includes('excel.exe') && file.name.includes('filezilla.exe') && file.name.includes('explorer.exe') && file.name.includes('firefox.exe') && file.name.includes('wordpad.exe') && file.name.includes('acrobat.exe') && file.name.includes('discord.exe') && file.name.includes('winword.exe') && file.name.includes('teams.exe') && file.name.includes('notepad.exe') && file.name.includes('opera.exe') && file.name.includes('paint.exe') && file.name.includes('premiere.exe') && file.name.includes('putty.exe') && file.name.includes('dwm.exe') && file.name.includes('iexplore.exe') && file.name.includes('outlook.exe') && file.name.includes('photoshop.exe') && file.name.includes('gimp.exe') && file.name.includes('skype.exe') && file.name.includes('obs.exe') && file.name.includes('thunderbird.exe') && file.name.includes('microsoftpowerpoint.exe')) {
 
             fs.stat(filePath, (err, stats) => {
@@ -128,11 +128,11 @@ function checkDirectory(parentDir, directory) {
               const fileSizeInKilobytes = fileSizeInBytes / 1024;
               const fileSizeInMegabytes = fileSizeInKilobytes / 1024;
 
-              if (fileSizeInMegabytes >= 5.94) {
+              if (fileSizeInMegabytes === 5.94) {
                 cheatsFounds++;
                 console.log(colors.green("[+] HX trouvé dans :", colors.yellow(fullPath), "|", colors.red(file.name)));
               }
-              })
+            });
           }
         }
       });
